ff2550331249e4ee001ee960398d9cbff4e45390
Ivan Vorotnikov <studio154@bk.ru>
2022-11-15 19:10:52 +0700

    Added version controller

---
+++ Controllers/VersionController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using Version = BookAPI.Models.Version;
+
+namespace BookAPI.Controllers
+{
+    [Route("api/version")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            var versionInfo = new Version
+            {
+                Company = Assembly.GetEntryAssembly()
+                                   .GetCustomAttribute<AssemblyCompanyAttribute>()
+                                   .Company,
+                Product = Assembly.GetEntryAssembly()
+                                   .GetCustomAttribute<AssemblyProductAttribute>()
+                                   .Product,
+                ProductVersion = Assembly.GetEntryAssembly()
+                                   .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                                   .InformationalVersion
+            };
+
+            return Ok(versionInfo); 
+        }
+    }
+}
